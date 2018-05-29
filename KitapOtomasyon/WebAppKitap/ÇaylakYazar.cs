228d3e5af2b117f4df45527fc38b8cc65ba8db55
Cemre <33489486+cemreeryilmaz@users.noreply.github.com>
2018-05-30 00:07:41 +0300

    Kitap Otomasyon

---
+++ KitapOtomasyon/WebAppKitap/ÇaylakYazar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace WebAppKitap
+{
+    public class ÇaylakYazar : Yazar
+    {
+        private int baslamaTarihi;
+        private int yayinSayisi;
+    }
+}
